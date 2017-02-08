4bb3b7f327ee51adba6321a8f34da916846b4f50
Brendon Steele <brendon@visionthree.com>
2017-02-08 17:26:07 -0500

    A clean repo son.  Brand new.

---
+++ Assets/JsonDataTypes/SerialisedMap.cs
@@ -0,0 +1,9 @@
+﻿class SerialisedMap
+{
+    public enum MapType { Moon, Asteroid, Urban};
+
+    public string name;
+    public MapType type;
+    public int width;
+    public int height;
+}
---
+++ Assets/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour {
+
+	void Start () {
+        LoadJson();
+	}
+	
+	void Update () {
+		
+	}
+
+    public void LoadJson()
+    {
+        string path = System.IO.Path.Combine("JsonData/Maps", "test_map");
+        TextAsset asset = Resources.Load(path) as TextAsset;
+        SerialisedMap map = JsonUtility.FromJson<SerialisedMap>(asset.text);
+        Debug.Log(":p");
+        Debug.Log(JsonUtility.ToJson(map));
+    }
+}
